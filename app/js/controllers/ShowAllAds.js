8b1adef3e133ff79fee0c03e1edc7f5017450316
morsky <vladvv2m@gmail.com>
2015-01-04 16:49:02 +0200

    Filters by catecory and by town are ready and works.

--- app/js/controllers/ShowAllAds.js
+++ app/js/controllers/ShowAllAds.js
@@ -1,17 +1,48 @@
-app.controller('ListAds', ['serviceHandler', function(serviceHandler) {
+app.controller('ListAds', ['$http', '$log', 'serviceHandler', function($http, $log, serviceHandler) {
 	var self = this;
 
 	// self.name = 'Vlado';
 
 	serviceHandler.getAllAds(function(response) {
 		self.data = response;
-	})
+	});
 
 	serviceHandler.getAllCategories(function(response) {
 		self.categories = response;
-	})
+	});
 
 	serviceHandler.getAllTowns(function(response) {
 		self.towns = response;
-	})
+	});
+
+	self.takeCategoryId = function(id) {
+		self.itemId = id;
+		console.log(self.itemId);
+
+		// $http({method: 'GET',
+		// 	url: 'http://softuni-ads.azurewebsites.net/api/ads',
+		// 	params: { 'categoryid': id }
+		// })
+		// .success(function(data) {
+		// 	// success(data);
+		// 	self.data = data;
+		// 	$log.log(data);
+		// })
+		// .error(function(data, status) {
+		// 	$log.warn(data);
+		// });
+
+		serviceHandler.getCategoryAds(id, function(response) {
+			self.data = response;
+		});
+	};
+
+	self.takeTownId = function(id) {
+		self.itemId = id;
+		console.log(self.itemId);
+
+		serviceHandler.getTownAds(id, function(response) {
+			self.data = response;
+		});
+	};
 }]);
--- app/js/services/httpServices.js
+++ app/js/services/httpServices.js
@@ -2,10 +2,38 @@ app.factory('serviceHandler', function($http, $log) {
 	return {
 		getAllAds: function(success) {
 			$http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/ads'})
-			.success(function(data, status, headers, config) {
+			.success(function(data) {
 				success(data);
 			})
-			.error(function(data, status, headers, config) {
+			.error(function(data, status) {
+				$log.warn(data);
+			})
+		},
+
+		getCategoryAds: function(category, success) {
+			$http({method: 'GET',
+				url: 'http://softuni-ads.azurewebsites.net/api/ads',
+				params: { 'categoryid': category }
+			})
+			.success(function(data) {
+				success(data);
+				$log.log(data);
+			})
+			.error(function(data, status) {
+				$log.warn(data);
+			})
+		},
+
+		getTownAds: function(town, success) {
+			$http({method: 'GET',
+				url: 'http://softuni-ads.azurewebsites.net/api/ads',
+				params: { 'townid': town }
+			})
+			.success(function(data) {
+				success(data);
+				$log.log(data);
+			})
+			.error(function(data, status) {
 				$log.warn(data);
 			})
 		},
